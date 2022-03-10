697d04a12b05aa8405d72cf7c53c113f6d1cb21a
arjuncv1993 <arjuncv110693@gmail.com>
2022-03-10 14:26:33 +0530

    update

--- myshoppe/myshoppe/settings.py
+++ myshoppe/myshoppe/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$eb7tui+9tutwdskm8aq@$u2%$+=4ycfmp%8o%u#8v043r!2#)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','newsite123.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
